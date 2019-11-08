227be4944a335953ea06544ee263c87025d5df79
Francisco Molina <molinajimenez01@gmail.com>
2019-11-08 00:18:16 -0600

    navbar decente

--- src/Intro.svelte
+++ src/Intro.svelte
@@ -1,5 +1,9 @@
 <script>
+    let activate = false;
 
+    function handleClick(){
+       activate = !activate;
+    }
 
 </script>
 
@@ -29,10 +33,10 @@
         left: 0;
         /*right: 0;*/
         margin: 0;
-        margin-top: 7%;
+        margin-top: 10%;
         height: 95vh;
         max-width: 300px;
-        width: 10%;
+        width: 12vw;
         padding: 0;
         text-align: center;
         list-style-type: none;
@@ -43,9 +47,20 @@ ul.controller{
     flex-direction: column;
     justify-content: flex-start;
     background: #374046;
-    max-height: fit-content;
+    max-height: 21.4vh;
     margin: 0;
     margin-bottom: 5%;
+    transition: 0.5s background ease-in;
+}
+
+ul.Activecontroller{
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    background: #374046;
+    max-height: 21.4vh;
+    margin: 0;
+    transition: 0.2s background ease-in;
     
 }
 
@@ -64,6 +79,28 @@ ul.controller a{
     margin:0;
     margin-top: 10%;
     margin-bottom: 5%;
+    max-height: 64px;
+    
+}
+
+ul.controller a i{
+    transition: 0.45s transform ease-in-out; 
+}
+
+ul.Activecontroller a i{
+    transform: rotate(180deg);
+    transition: 0.45s transform ease-in-out; 
+}
+
+ul.Activecontroller a{
+    margin-bottom: 100%;
+    color: white;
+    font-size: 54px;
+    margin:0;
+    margin-top: 10%;
+    margin-bottom: 5%;
+    max-height: 64px;
+    
 }
 ul a {
     margin: 45px 0;
@@ -80,16 +117,32 @@ a i{
     font-size: 54px;
 }
 
+ul.sideNav{
+    left: -12vw;
+    transition: 0.5s left ease-in;
+    
+}
+
+ul.ActivesideNav{
+    left: 0;
+    transition: 0.5s left ease-in;
+}
+
+i:hover{
+    opacity: 0.6;
+    transition: 0.2s opacity ease-in-out;
+}
+
 
 </style>
-<section>
-    <ul class="controller">
-        <a id="uniqueHamburguer" href="#t1"><i  class="la la-bars"></i></a>
+<section style="position:relative;">
+    <ul class='{activate ? "Activecontroller" : "controller" }'>
+        <a id="uniqueHamburguer" href="#t1"><i  class="la la-bars" on:click={handleClick}></i></a>
     </ul>
     
-    <ul>
+    <ul class='{activate ? "ActivesideNav" : "sideNav"}'>
         <a href="#t1"><i class="la la-male"></i></a>
-        <a href="#t1"><i class="la la-terminal"></i></a>
+        <a href="/terminal"><i class="la la-terminal"></i></a>
         <a href="#t1"><i class="la la-university"></i></a>
         <a href="#t1"><i class="la la-puzzle-piece"></i></a>
     </ul>
